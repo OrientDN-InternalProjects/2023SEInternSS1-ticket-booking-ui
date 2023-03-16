9ea3fb7d5dc9c3295f2372a23243626a7daa95af
ngqtrieu99 <nguyenquangtrieu99@gmail.com>
2023-03-16 12:28:19 +0700

    Add contract detail

--- bookingticketui/src/components/ContactDetail/ContactForm.jsx
+++ bookingticketui/src/components/ContactDetail/ContactForm.jsx
@@ -1,35 +1,54 @@
+import axios from "axios";
 import React, { useState} from 'react'
 import './ContactForm.css' 
 
+const err = "Error has occured";
+
 const ContactForm = () => {
     const [formStatus, setFormStatus] = useState('Send')
-    const onSubmit = (e) => {
-      e.preventDefault()
-      setFormStatus('Submitting...')
-      const { FirstName, MiddleName, LastName, PhoneNumber, Email } = e.target.elements
-      let conFom = {
-        fname: FirstName?.value,
-        mname: MiddleName?.value,
-        lname: LastName?.value,
-        phone: PhoneNumber?.value,
-        mail: Email?.value,
-      }
+    const [ conFom, setConFom ] = useState({
+      firstName: "",
+      middleName: "",
+      lastName: "",
+      email: "",
+      phoneNumber: ""
+    })
 
-      console.log(conFom)
+    const handleChange  = (event) => {
+      setConFom({
+        ...conFom,
+        [event.target.name]: event.target.value
+      })
     }
+
+    const submitForm = async (event) => {
+      event.preventDefault()
+      try {
+        await axios.post("https://localhost:7089/api/Booking/request-contact", {
+          firstName: conFom.firstName,
+          middleName: conFom.middleName,
+          lastName: conFom.lastName,
+          email: conFom.email,
+          phoneNumber: conFom.phoneNumber
+        })
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
     return (
       <div style={{ display: "flex", width: "1920px", justifyContent: 'center' }}>
       <div className="container">
         <h1 id = "contactheader">Contact details</h1>
 
-        <form onSubmit={onSubmit}>
+        <form onSubmit={submitForm}>
 
           {/* First Name form   */}
           <div className="mb-3">
             <label className="form-label" htmlFor="FirstName">
               First Name
             </label>
-            <input className="form-control" type="text" value = {formStatus.FirstName} id="FirstName"  required />
+            <input className="form-control" type="text" name="firstName" id="FirstName" onChange={handleChange} required />
           </div>
 
 
@@ -38,7 +57,7 @@ const ContactForm = () => {
             <label className="form-label" htmlFor="MiddleName">
               Middle Name
             </label>
-            <input className="form-control" type="text" value = {formStatus.MiddleName} id="MiddleName" required />
+            <input className="form-control" type="text" name="middleName" id="MiddleName" onChange={handleChange} required />
           </div>
 
           {/* Last Name form   */}
@@ -46,7 +65,7 @@ const ContactForm = () => {
             <label className="form-label" htmlFor="LastName">
               Last Name
             </label>
-            <input className="form-control" type="text" value = {formStatus.LastName} id="LastName" required />
+            <input className="form-control" type="text" name="lastName" id="LastName" onChange={handleChange} required />
           </div>
 
 
@@ -54,7 +73,7 @@ const ContactForm = () => {
             <label className="form-label" htmlFor="email">
               Email
             </label>
-            <input className="form-control" type="email" pattern=".+@gmail\.com" value = {formStatus.Email} id="Email" required />
+            <input className="form-control" type="email" name="email" pattern=".+@gmail\.com" onChange={handleChange} id="Email" required />
           </div>
 
 
@@ -62,11 +81,13 @@ const ContactForm = () => {
             <label className="form-label" htmlFor="Phone">
               Phone Number
             </label>
-            <input className="form-control" type="tel" value = {formStatus.PhoneNumber} id="PhoneNumber" required />
+            <input className="form-control" type="tel" name="phoneNumber" id="PhoneNumber" onChange={handleChange} required />
           </div>
 
 
-          <button className="btn btn-danger" type="submit">
+          <button className="btn btn-danger" 
+                  type="submit"
+                  >
             {formStatus}
           </button>
         </form>
