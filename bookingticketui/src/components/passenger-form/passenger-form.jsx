3c8ac1de590ee64efb128579aefcb7bd4805d9d5
Thinh Huynh <hnthinh55@gmail.com>
2023-03-23 15:35:34 +0700

    fix CountryDropdown

--- bookingticketui/src/components/passenger-form/passenger-form.jsx
+++ bookingticketui/src/components/passenger-form/passenger-form.jsx
@@ -1,61 +1,136 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
+import {
+  CountryDropdown,
+  RegionDropdown,
+  CountryRegionData,
+} from "react-country-region-selector";
 
 const Passenger = () => {
+  const [inputFields, setInputFields] = useState([
+    {
+      firstName: "",
+      lastName: "",
+      dateOfBirth: "",
+      nation: "",
+      identifyCard: "",
+      provideNa: "",
+      expDate: "",
+    },
+  ]);
+
+  const [country, setCountry] = useState("");
+
+  const handleFormChange = (index, event) => {
+    let data = [...inputFields];
+    data[index][event.target.name] = event.target.value;
+    setInputFields(data);
+  };
+
+  const addFields = () => {
+    let newfield = {
+      firstName: "",
+      lastName: "",
+      dateOfBirth: "",
+      nation: "",
+      identifyCard: "",
+      provideNa: "",
+      expDate: "",
+    };
+
+    setInputFields([...inputFields, newfield]);
+  };
   return (
     <Container className="w-50 p-3 mb-2 bg-light text-dark">
       <Form>
         <h1>Passenger details</h1>
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label>First Name</Form.Label>
-            <Form.Control type="text" placeholder="First Name" />
-          </Form.Group>
+        {inputFields.map((input, index) => {
+          return (
+            <Form key={index}>
+              <Row className="mb-3">
+                <Form.Group as={Col} controlId="formGridEmail">
+                  <Form.Label>First Name</Form.Label>
+                  <Form.Control
+                    name="firstName"
+                    type="text"
+                    placeholder="First Name"
+                    value={input.firstName}
+                    onChange={(event) => handleFormChange(index, event)}
+                  />
+                </Form.Group>
 
-          <Form.Group as={Col} controlId="formGridPassword">
-            <Form.Label>Last Name</Form.Label>
-            <Form.Control type="text" placeholder="Last Name" />
-          </Form.Group>
-        </Row>
+                <Form.Group as={Col} controlId="formGridPassword">
+                  <Form.Label>Last Name</Form.Label>
+                  <Form.Control
+                    name="lastName"
+                    type="text"
+                    placeholder="Last Name"
+                    value={input.lastName}
+                    onChange={(event) => handleFormChange(index, event)}
+                  />
+                </Form.Group>
+              </Row>
 
-        <Form.Group className="mb-3" controlId="formGridAddress1">
-          <Form.Label>Date of birth</Form.Label>
-          <Form.Control placeholder="1234 Main St" />
-        </Form.Group>
+              <Form.Group className="mb-3" controlId="formGridAddress1">
+                <Form.Label>Date of birth</Form.Label>
+                <Form.Control
+                  name="dateOfBirth"
+                  placeholder="MM/dd/YYYY: 03/23/2023"
+                  value={input.dateOfBirth}
+                  onChange={(event) => handleFormChange(index, event)}
+                />
+              </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formGridAddress2">
-          <Form.Label>Nation</Form.Label>
-          <Form.Control />
-        </Form.Group>
+              <Form.Group className="mb-3" controlId="formGridAddress2">
+                <Form.Label>Nation</Form.Label>
+                <CountryDropdown
+                  name="nation"
+                  value={country}
+                  // onChange={(event) => handleFormChange(index, event)}
+                  onChange={(val) => setCountry(val)}
+                />
+              </Form.Group>
 
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridCity">
-            <Form.Label>IdentityCard</Form.Label>
-            <Form.Control />
-          </Form.Group>
+              <Row className="mb-3">
+                <Form.Group as={Col} controlId="formGridCity">
+                  <Form.Label>IdentityCard</Form.Label>
+                  <Form.Control
+                    name="identifyCard"
+                    value={input.identifyCard}
+                    onChange={(event) => handleFormChange(index, event)}
+                  />
+                </Form.Group>
 
-          <Form.Group as={Col} controlId="formGridState">
-            <Form.Label>ProvideNa</Form.Label>
-            <Form.Control />
-          </Form.Group>
+                <Form.Group as={Col} controlId="formGridState">
+                  <Form.Label>ProvideNa</Form.Label>
+                  <Form.Control
+                    name="provideNa"
+                    value={input.provideNa}
+                    onChange={(event) => handleFormChange(index, event)}
+                  />
+                </Form.Group>
 
-          <Form.Group as={Col} controlId="formGridZip">
-            <Form.Label>ExpDate</Form.Label>
-            <Form.Control />
-          </Form.Group>
-        </Row>
-
-        <Form.Group className="mb-3" id="formGridCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
+                <Form.Group as={Col} controlId="formGridZip">
+                  <Form.Label>ExpDate</Form.Label>
+                  <Form.Control
+                    name="expDate"
+                    value={input.expDate}
+                    onChange={(event) => handleFormChange(index, event)}
+                  />
+                </Form.Group>
+              </Row>
+              <Form>
+                <Button variant="primary" onClick={addFields}>
+                  add field
+                </Button>
+              </Form>
+            </Form>
+          );
+        })}
       </Form>
     </Container>
   );
--- bookingticketui/src/components/sign-up/sign-up.jsx
+++ bookingticketui/src/components/sign-up/sign-up.jsx
@@ -1,24 +1,29 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { createAuthenticateAPIEndpoint, ENDPOINTS } from "/src/api/AuthenticateAPI";
-import { validateEmail, validatePassword, validateConfirmPassword } from "/src/components/sign-up/sign-up-validation";
+import {
+  createAuthenticateAPIEndpoint,
+  ENDPOINTS,
+} from "/src/api/AuthenticateAPI";
+import {
+  validateEmail,
+  validatePassword,
+  validateConfirmPassword,
+} from "/src/components/sign-up/sign-up-validation";
 import { useFormik, withFormik } from "formik";
+import { Container } from "react-bootstrap";
 import * as Yup from "yup";
-import "./sign-up.css"
-
-
 const SignUpForm = () => {
   const [formStatus, setFormStatus] = useState("Send");
   const [focused, setFocused] = useState(false);
   const [errorMessage, setErrMsg] = useState();
   const conForm = useFormik({
     initialValues: {
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-    isAdmin: false
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      isAdmin: false,
     },
 
     validationSchema: Yup.object({
@@ -38,156 +43,158 @@ const SignUpForm = () => {
         .max(24, "Maximun 24 characters")
         .matches(
           /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{6,})/,
-          "Must Contain at least 6 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character")
+          "Must Contain at least 6 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+        )
         .required("Password is required!"),
       confirmPassword: Yup.string()
         .oneOf([Yup.ref("password")], "Password's not match")
-        .required("Please re-enter password!")
-    })
+        .required("Please re-enter password!"),
+    }),
   });
   const submitForm = async (event) => {
     event.preventDefault();
     console.log(conForm.values.confirmPassword);
-    if (!validateEmail(conForm.values.email)  
-        || !validatePassword(conForm.values.password)
-        || !validateConfirmPassword(conForm.values.password, conForm.values.confirmPassword)) {
-        alert("Register failed");
+    if (
+      !validateEmail(conForm.values.email) ||
+      !validatePassword(conForm.values.password) ||
+      !validateConfirmPassword(
+        conForm.values.password,
+        conForm.values.confirmPassword
+      )
+    ) {
+      alert("Register failed");
     }
 
-    const response = await createAuthenticateAPIEndpoint(ENDPOINTS.signup).post({
-      firstName: conForm.values.firstName,
-      lastName: conForm.values.lastName,
-      email: conForm.values.email,
-      password: conForm.values.password,
-      confirmPassword: conForm.values.confirmPassword,
-      isAdmin: conForm.values.isAdmin,
-    });
+    const response = await createAuthenticateAPIEndpoint(ENDPOINTS.signup).post(
+      {
+        firstName: conForm.values.firstName,
+        lastName: conForm.values.lastName,
+        email: conForm.values.email,
+        password: conForm.values.password,
+        confirmPassword: conForm.values.confirmPassword,
+        isAdmin: conForm.values.isAdmin,
+      }
+    );
 
-    if(response.data.result?.status == true)
-    {
+    if (response.data.result?.status == true) {
       // We intent to redirect to the login page
       // this block will be added after finish login page
+    } else {
+      alert(response.data.result?.message);
     }
-
-    else
-    {
-      alert(response.data.result?.message)
-    }
-    
   };
 
-
   return (
-    <div>
-      <div className="container">
-        <h1 id="contactheader">SIGN UP FORM</h1>
-        
-        <div style={{
+    <Container className="w-25 p-3 mb-2 bg-light text-dark">
+      <h1 id="contactheader">SIGN UP FORM</h1>
+
+      <div
+        style={{
           display: "flex",
-          justifyContent: "center"
-        }}>
-          <form onSubmit={submitForm}>
-            {/* First Name form   */}
-            <div className="mb-3">
-              <label className="form-label" htmlFor="FirstName">
-                First Name
-              </label>
+          justifyContent: "center",
+        }}
+      >
+        <form onSubmit={submitForm}>
+          {/* First Name form   */}
+          <div className="mb-3">
+            <label className="form-label" htmlFor="FirstName">
+              First Name
+            </label>
+            <input
+              className="form-control"
+              type="text"
+              name="firstName"
+              id="firstName"
+              value={conForm.values.firstName}
+              onChange={conForm.handleChange}
+              required
+            />
+            <span>{conForm.errors.firstName}</span>
+          </div>
+
+          {/* Last Name form   */}
+          <div className="mb-3">
+            <label className="form-label" htmlFor="LastName">
+              Last Name
+            </label>
+            <input
+              className="form-control"
+              type="text"
+              name="lastName"
+              id="lastName"
+              value={conForm.values.lastName}
+              onChange={conForm.handleChange}
+              required
+            />
+            <span>{conForm.errors.lastName}</span>
+          </div>
+
+          <div className="mb-3">
+            <label className="form-label" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="form-control"
+              type="email"
+              name="email"
+              id="email"
+              value={conForm.values.email}
+              onChange={conForm.handleChange}
+              required
+            />
+            <span>{conForm.errors.email}</span>
+          </div>
+
+          <div className="mb-3">
+            <label className="form-label" htmlFor="password">
+              Password
+            </label>
+            <div>
               <input
-                className="form-control"
-                type="text"
-                name="firstName"
-                id="firstName"
-                value = {conForm.values.firstName}
+                style={{
+                  borderRadius: "5px",
+                  border: "none",
+                }}
+                className="form-input"
+                type="password"
+                name="password"
+                id="password"
+                value={conForm.values.password}
                 onChange={conForm.handleChange}
                 required
               />
-              <span>{conForm.errors.firstName}</span>
             </div>
+            <span>{conForm.errors.password}</span>
+          </div>
 
-            {/* Last Name form   */}
-            <div className="mb-3">
-              <label className="form-label" htmlFor="LastName">
-                Last Name
-              </label>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="confirmPassword">
+              Confirm Password
+            </label>
+            <div>
               <input
-                className="form-control"
-                type="text"
-                name="lastName"
-                id="lastName"
-                value = {conForm.values.lastName}
+                style={{
+                  borderRadius: "5px",
+                  border: "none",
+                }}
+                className="form-input"
+                type="password"
+                name="confirmPassword"
+                id="confirmPassword"
+                value={conForm.values.confirmPassword}
                 onChange={conForm.handleChange}
                 required
               />
-              <span>{conForm.errors.lastName}</span>
             </div>
+            <span>{conForm.errors.confirmPassword}</span>
+          </div>
 
-            <div className="mb-3">
-              <label className="form-label" htmlFor="email">
-                Email
-              </label>
-              <input
-                className="form-control"
-                type="email"
-                name="email"
-                id="email"
-                value = {conForm.values.email}
-                onChange={conForm.handleChange}
-                required
-              />
-              <span>{conForm.errors.email}</span>
-            </div>
-
-            <div className="mb-3">
-              <label className="form-label" htmlFor="password">
-                Password
-              </label>
-              <div>
-                <input
-                  style={{
-                    borderRadius: "5px",
-                    border: "none"
-                  }}
-                  className="form-input"
-                  type="password"
-                  name="password"
-                  id="password"
-                  value = {conForm.values.password}
-                  onChange={conForm.handleChange}
-                  required
-                />
-              </div>
-              <span>{conForm.errors.password}</span>
-            </div>
-
-            <div className="mb-3">
-              <label className="form-label" htmlFor="confirmPassword">
-                Confirm Password
-              </label>
-              <div>
-                <input
-                  style={{
-                    borderRadius: "5px",
-                    border: "none"
-                  }}
-                  className="form-input"
-                  type="password"
-                  name="confirmPassword"
-                  id="confirmPassword"
-                  value = {conForm.values.confirmPassword}
-                  onChange={conForm.handleChange}
-                  required
-                />
-              </div>
-              <span>{conForm.errors.confirmPassword}</span>
-            </div>
-
-            <button className="btn btn-danger" type="submit">
-              {formStatus}
-            </button>
-          </form>
-        </div>
+          <button className="btn btn-danger" type="submit">
+            {formStatus}
+          </button>
+        </form>
       </div>
-    </div>
+    </Container>
   );
 };
 
